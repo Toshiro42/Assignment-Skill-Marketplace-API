908b0aeca88a3b270c7570b58cd0b56221b78a74
Toshiro42 <suyashyogi42@gmail.com>
2026-04-21 20:05:39 +0530

    Add Spring Boot application entry point

---
+++ src/main/java/com/Skill/Marketplace/SM/SmApplication.java
@@ -0,0 +1,15 @@
+package com.Skill.Marketplace.SM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmApplication.class, args);
+    }
+
+}
